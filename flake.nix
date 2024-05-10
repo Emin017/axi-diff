a265c81a1939328748da40b35ec67d5b2fcdf258
Qiming Chu <cchuqiming@gmail.com>
2024-05-10 17:02:23 +0800

    Add graalvm21 for mill

--- flake.nix
+++ flake.nix
@@ -4,21 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-old.url = "github:NixOS/nixpkgs/336eda0d07dc5e2be1f923990ad9fdb6bc8e28e3";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    nixpkgs-old,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+          config.packageOverrides = pkgs: {
+            graalvm = nixpkgs-old.legacyPackages.${system}.graalvm-ce;
+          };
+        };
         deps = with pkgs; [
           git
-          jdk21
-          mill
+          (mill.override {jre = pkgs.graalvm;})
           gnumake
           autoconf
           automake
